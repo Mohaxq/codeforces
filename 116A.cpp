8792b0d54f5fbd71903f5d23e0c579678b323527
Mohamed <159513882+Mohaxq@users.noreply.github.com>
2024-10-21 20:55:31 +0300

    Add files via upload

---
+++ 116A.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main (){
+
+int t,cap=0,maxe=0;
+
+cin>>t;                      // enter test case
+
+while (t--){
+        int i,o;
+
+    cin>>o>>i;             // enter number for people in and out
+    cap +=i;
+    cap -=o;
+
+    maxe = max(maxe,cap); // compare between the cap and maxe
+                         // and found the max value
+
+}
+
+cout<<maxe;
+}
